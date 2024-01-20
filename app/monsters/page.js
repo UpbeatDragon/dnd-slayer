b6ba5a7ece4e0030ec19280cfdbecd09b1e058c0
UpBeatDragon <alice.c2007@outlook.com>
2024-01-20 13:43:38 +0000

    Added back button

--- app/monsters/page.js
+++ app/monsters/page.js
@@ -1,9 +1,12 @@
+import Link from "next/link"
+
 export default async function Spells() {
 
     const res = await fetch("https://www.dnd5eapi.co/api/monsters")
     const allMonsters = await res.json()
     return(
         <>
+        <Link href={'/'}>Home</Link>
         <h1 className="text-2xl text-red-500">Monsters!</h1>
         <ul>
             {
--- app/spells/page.js
+++ app/spells/page.js
@@ -1,9 +1,12 @@
+import Link from "next/link"
+
 export default async function Spells() {
 
     const res = await fetch("https://www.dnd5eapi.co/api/spells")
     const allSpells = await res.json()
     return(
         <>
+        <Link href={'/'}>Home</Link>
         <h1 className="text-2xl text-red-500">Spells!</h1>
         <ul>
             {
