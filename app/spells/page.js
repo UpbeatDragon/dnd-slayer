36c92b35649ac1a1aa0671ff2fbcfe730a04b435
UpBeatDragon <alice.c2007@outlook.com>
2024-01-20 13:18:34 +0000

    Added spells page and link

---
+++ app/spells/page.js
@@ -0,0 +1,18 @@
+export default async function Spells() {
+
+    const res = await fetch("https://www.dnd5eapi.co/api/spells")
+    const allSpells = await res.json()
+    return(
+        <>
+        <h1 className="text-2xl text-red-500">Spells!</h1>
+        <ul>
+            {
+                allSpells.results.map(
+                    s=>(<li key={s.index}>{s.name}</li>)
+                )
+            }
+        </ul>
+        
+        </>
+    )
+}
