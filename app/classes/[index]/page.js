aeef9d8c87433f995ef7ce7cc3eb2ec5ad4fee82
UpBeatDragon <alice.c2007@outlook.com>
2024-01-21 09:42:17 +0000

    Added some descs for classes

--- app/classes/[index]/page.js
+++ app/classes/[index]/page.js
@@ -1,5 +1,14 @@
 export default async function Class({params}) {
+    const res = await fetch(`https://www.dnd5eapi.co/api/classes/${params.index}`)
+    const classInfo = await res.json()
     return(
+        <>
         <h1>Hello {params.index}</h1>
+        <pre>{JSON.stringify(classInfo,null,2)}</pre>
+        </>
     )
+        
+        
+   
+    
 }
