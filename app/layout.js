e8e8fafba5cfea3f304d9a2f27d53d0addc7ace3
UpBeatDragon <alice.c2007@outlook.com>
2024-01-21 09:01:56 +0000

    added proper menu (thanks dad)

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Link from 'next/link'
+import TitleBar from './components/title-bar/title-bar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,13 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ul className="inline-flex pb-6 text-xl ">
-        <li className="pr-6 pl-6"><Link href={'/'} className="hover:text-red-500">Home</Link></li>
-        <li className="pr-6"><Link href={'/spells'} className="hover:text-red-500">Spells</Link></li>
-        <li className="pr-6"><Link href={'/monsters'} className="hover:text-red-500">Monsters</Link></li>
-        <li className="pr-6"><Link href={'/races'} className="hover:text-red-500">Races</Link></li>
-        <li className="pr-6"><Link href={'/classes'} className="hover:text-red-500">Classes</Link></li>
-        </ul>
+        <TitleBar></TitleBar>
         <>
         {children}
         </>
