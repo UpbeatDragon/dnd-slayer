a145e7e510936a0fff92649a90d3769b5d5b32f8
UpBeatDragon <alice.c2007@outlook.com>
2024-01-21 15:07:13 +0000

    Made more responsive stuff innit

--- app/classes/page.js
+++ app/classes/page.js
@@ -7,10 +7,10 @@ export default async function Classes() {
     return(
         <>
         <h1 className="text-2xl text-red-500">Classes!</h1>
-        <div className="grid-cols-3 grid gap-4">
+        <div className="grid-cols-2 grid gap-4 lg:grid-cols-3">
         {
                 allClasses.results.map(
-                    s=>(<Link key={s.index} href={`/classes/${s.index}`} className="hover:text-red-500">{s.name}</Link>)
+                    s=>(<div><Link key={s.index} href={`/classes/${s.index}`} className="hover:text-red-500">{s.name}</Link></div>)
                 )
             }
         </div>
